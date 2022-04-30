070f1e977c312823f5bfa3b9c2f1e8ca19590864
Utkarsh Mishra <Utkarshmishra73444@gmail.com>
2022-04-30 11:08:46 +0530

    initial

---
+++ pages/_middleware.ts
@@ -0,0 +1,12 @@
+import { NextResponse } from "next/server";
+
+const signedInPages = ["/", "/playlist", "/library"];
+
+export default function middleware(req) {
+  if (signedInPages.find((p) => p === req.nextUrl.pathname)) {
+    const token = req.cookies.MUSIFY_ACCESS_TOKEN;
+    if (!token) {
+      return NextResponse.redirect("/signin");
+    }
+  }
+}
